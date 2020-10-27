5acb06d2c69520021e94f7a90cdb44640632d992
KirillLutsenko <mnrovski68@gmail.com>
2020-10-27 15:01:49 +0200

    Updates

---
+++ precache-manifest.de0eaf401bbfe6da5c7ef46083696a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c836e6ea67fb04197ceb6aa2c2b8bce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "806630b3cfae3680e18a",
+    "url": "./static/css/main.61ef100b.chunk.css"
+  },
+  {
+    "revision": "ae512d195d694729d30e",
+    "url": "./static/js/2.0f213038.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0f213038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806630b3cfae3680e18a",
+    "url": "./static/js/main.48903e90.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
